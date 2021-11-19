62b630ccf6ac9ed85dcf638360eccd41800648e2
Rafael Aponte III <raponte0044@gmail.com>
2021-11-19 03:03:05 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var img = document.getElementById("side-img2");
+
+img.addEventListener("mouseenter", function() {
+    this.src = "elon-spacex.jpg";
+}, false);
+
+img.addEventListener("mouseleave", function() {
+    this.src = "musktesla.jpg";
+}, false);
+
+img.addEventListener("mouseenter", function() {
+    this.src = "musktesla.jpg";
+}, false);
+
+img.addEventListener("mouseleave", function() {
+    this.src = "elon-spacex.jpg";
+}, false);
+
+
+
+var img = document.getElementById("side-img1");
+
+img.addEventListener("mouseenter", function() {
+    this.src = "muskzip2.webp";
+}, false);
+
+img.addEventListener("mouseleave", function() {
+    this.src = "muskx.webp";
+}, false);
+
+img.addEventListener("mouseenter", function() {
+    this.src = "muskx.webp";
+}, false);
+
+img.addEventListener("mouseleave", function() {
+    this.src = "muskzip2.webp";
+}, false);
